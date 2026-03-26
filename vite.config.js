4793e591ffeef8e94c1793ff6013799804017b37
Shahar <7824zj7npd@privaterelay.appleid.com>
2026-03-26 12:35:19 +0200

    Update build files and config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/ChessWeb/',
+  base: process.env.NODE_ENV === 'production' ? '/ChessWeb/' : '/',
   server: {
     proxy: {
       "/api": "http://localhost:3000",
